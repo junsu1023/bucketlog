ea814f3ee28b95a4b83faf937054f8418ec9cf18
junsu1023 <jangdokg12@naver.com>
2026-08-17 18:51:46 +0900

    feat: Koin DI에 사진 파이프라인 배선
    ImageProcessor(무인자, 양쪽 동일)는 공용 모듈에 등록. FileStorage는
    DatabaseFactory와 같은 이유로 Android는 Context가 필요해 플랫폼별
    platformModule에서 각각 등록(BucketLogApplication / MainViewController).
    AddProgressEntryUseCase와 CompleteGoalUseCase(사진 파라미터 추가)도 등록.

--- shared/src/commonMain/kotlin/com/bucketlog/di/AppModule.kt
+++ shared/src/commonMain/kotlin/com/bucketlog/di/AppModule.kt
@@ -8,14 +8,15 @@ import com.bucketlog.domain.repository.EntryRepository
 import com.bucketlog.domain.repository.GoalRepository
 import com.bucketlog.domain.usecase.AddCheckInEntryUseCase
 import com.bucketlog.domain.usecase.AddGoalUseCase
+import com.bucketlog.domain.usecase.AddProgressEntryUseCase
 import com.bucketlog.domain.usecase.ArchiveGoalUseCase
 import com.bucketlog.domain.usecase.CompleteGoalUseCase
 import com.bucketlog.domain.usecase.DeleteGoalUseCase
 import com.bucketlog.domain.usecase.ObserveGoalOverviewsUseCase
 import com.bucketlog.domain.usecase.RestoreGoalUseCase
+import com.bucketlog.platform.ImageProcessor
 import com.bucketlog.presentation.addgoal.AddGoalViewModel
 import com.bucketlog.presentation.home.HomeViewModel
-import org.koin.compose.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.viewModelOf
@@ -28,15 +29,20 @@ val appModule = module {
     single { get<AppDatabase>().entryDao() }
     single { get<AppDatabase>().photoDao() }
 
-    single<GoalRepository> { GoalRepositoryImpl(get()) }
-    single<EntryRepository> { EntryRepositoryImpl(get()) }
+    // BucketLog: 사진 파이프라인(2주차). FileStorage는 Android/iOS 생성자가 달라
+    // platformModule에서 각각 등록한다(DatabaseFactory와 동일한 패턴).
+    single { ImageProcessor() }
+
+    single<GoalRepository> { GoalRepositoryImpl(get(), get(), get()) }
+    single<EntryRepository> { EntryRepositoryImpl(get(), get(), get()) }
 
     factory { AddGoalUseCase(get()) }
     factory { DeleteGoalUseCase(get()) }
-    factory { CompleteGoalUseCase(get(), get()) }
+    factory { CompleteGoalUseCase(get(), get(), get(), get()) }
     factory { ArchiveGoalUseCase(get()) }
     factory { RestoreGoalUseCase(get(), get()) }
     factory { AddCheckInEntryUseCase(get()) }
+    factory { AddProgressEntryUseCase(get(), get(), get()) }
     factory { ObserveGoalOverviewsUseCase(get(), get()) }
 
     viewModelOf(::HomeViewModel)
